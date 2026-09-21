da55099d6bfdc25d687cffe4028f6825f3cf7cca
angelroblegi <72879748+angelroblegi@users.noreply.github.com>
2026-09-21 14:21:41 -0500

    Remove token caching logic and add variable assignment

--- extraer_issues_html.py
+++ extraer_issues_html.py
@@ -1,14 +1,1 @@
-  const cachedToken = this.cache.get();
-    if (cachedToken) {
-      return cachedToken;
-    }
-    if (this.isTokenPromise) {
-Expected non-Promise value in a boolean conditional.
-      return this.isTokenPromise;
-    }
-    this.isTokenPromise = this.fetchTokenWithRetry();
-    try {
-      return await this.isTokenPromise;
-    } finally {
-      this.isTokenPromise = null;
-    }
+a = 1
